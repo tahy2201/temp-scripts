c9405fca7ec6980b1ed9b23e3ee9a0648b8a847d
tahy2201 <49649531+tahy2201@users.noreply.github.com>
2023-03-18 02:40:06 +0000

    first commit

---
+++ temp.py
@@ -0,0 +1,23 @@
+import os
+import openai
+
+openai.api_key = 'apikey'
+
+input_str = 'init'
+messages=[]
+
+while input_str != 'end':
+    input_str = input("chatgpt君に申したい事:")
+    if input_str == 'end':
+        break
+
+    messages.append({"role": "user", "content": input_str})
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = messages
+    )
+    res_message = response.choices[0]["message"]["content"].strip()
+    print(res_message)
+    messages.append({"role": "assistant", "content": res_message})
+    print(messages)
